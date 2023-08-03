b5e4ecfa394539979772569710f3842d277858ca
gangster <asliddinkarimboyev@gmail.com>
2023-08-03 16:14:14 +0500

    TODO spam module

---
+++ modules/spam/spam.js
@@ -0,0 +1,9 @@
+const telegram = require("telegram");
+/**
+ * @param {telegram.TelegramClient} client
+ * @param {telegram.client.message} msg
+ */
+
+module.exports["spam"] = async (client, msg, arg) => {
+    const count;
+};
