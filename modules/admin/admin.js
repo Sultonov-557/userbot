a1e4c6348ed7911908f067526ce429d7db2a7397
Sultonov-557 <sultonovzerifboy@gmail.com>
2024-01-22 15:40:53 +0300

    admin

--- modules/admin/admin.js
+++ modules/admin/admin.js
@@ -1,9 +1,81 @@
 const telegram = require("telegram");
 /**
  * @param {telegram.TelegramClient} client
- * @param {telegram.client.message} msg
+ * @param {telegram.Api.Message} msg
+ * @param {string} arg
  */
 
 module.exports["ban"] = async (client, msg, arg) => {
- 
+  const chatID = msg.chat.id;
+  const replyID = (await client.getMessages(chatID, { ids: msg.replyTo.replyToMsgId }))[0].sender.id;
+  await client.invoke(
+    new telegram.Api.channels.EditBanned({
+      channel: chatID,
+      participant: replyID,
+      bannedRights: new telegram.Api.ChatBannedRights({
+        viewMessages: false,
+      }),
+    })
+  );
+  msg.edit({ text: "banned" });
+};
+
+/**
+ * @param {telegram.TelegramClient} client
+ * @param {telegram.Api.Message} msg
+ * @param {string} arg
+ */
+module.exports["unban"] = async (client, msg, arg) => {
+  const chatID = msg.chat.id;
+  const replyID = (await client.getMessages(chatID, { ids: msg.replyTo.replyToMsgId }))[0].sender.id;
+  await client.invoke(
+    new telegram.Api.channels.EditBanned({
+      channel: chatID,
+      participant: replyID,
+      bannedRights: new telegram.Api.ChatBannedRights({
+        viewMessages: true,
+      }),
+    })
+  );
+  msg.edit({ text: "banned" });
+};
+
+/**
+ * @param {telegram.TelegramClient} client
+ * @param {telegram.Api.Message} msg
+ * @param {string} arg
+ */
+module.exports["mute"] = async (client, msg, arg) => {
+  const chatID = msg.chat.id;
+  const replyID = (await client.getMessages(chatID, { ids: msg.replyTo.replyToMsgId }))[0].sender.id;
+  client.invoke(
+    new telegram.Api.channels.EditBanned({
+      channel: chatID,
+      participant: replyID,
+      bannedRights: new telegram.Api.ChatBannedRights({
+        sendMessages: false,
+      }),
+    })
+  );
+  msg.edit({ text: "muted" });
+};
+
+/**
+ * @param {telegram.TelegramClient} client
+ * @param {telegram.Api.Message} msg
+ * @param {string} arg
+ */
+module.exports["unmute"] = async (client, msg, arg) => {
+  const chatID = msg.chat.id;
+  const replyID = (await client.getMessages(chatID, { ids: msg.replyTo.replyToMsgId }))[0].sender.id;
+  await client.invoke(
+    new telegram.Api.channels.EditBanned({
+      channel: chatID,
+      participant: replyID,
+      bannedRights: new telegram.Api.ChatBannedRights({
+        sendMessages: false,
+      }),
+    })
+  );
+  msg.edit({ text: "unmuted" });
 };
